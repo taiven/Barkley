b8d0215212561328f9fc98cf3209af880b135c6e
Taiven Rumph <wubcrate@gmail.com>
2014-09-20 17:42:24 -0400

    Updated Projects Class and added unit testing file.

--- bk-admin/includes/Project.php
+++ bk-admin/includes/Project.php
@@ -2,13 +2,58 @@
 
 class Project{
 	
-	private $_projectID;
+	public $projectID;
+	public $projectName;
+	public $projectStart;
+	public $projectDeadline;
+	public $projectDescription;
+	public $projectArchived;
 	
 	public function Create(){}
 	public function Update(){}
 	public function Delete(){}
 	public function Archive(){}
 	public function Restore(){}
+	
+	public function ReturnAll($AccountID = null){
+		$DataConnect = mysqli_connect('localhost','barkley','barkley','barkley');
+		if(isset($AccountID)){
+			$result = mysqli_query($DataConnect,"SELECT * FROM project_mapping, projects WHERE a_id = '$AccountID' AND p_id=project_id");
+		}else{
+			$result = mysqli_query($DataConnect,"SELECT * FROM projects WHERE 1");
+		}
+		$Projects = array();
+			while($row = mysqli_fetch_array($result)) {			
+					
+					$Project = new Project();
+					$Project->projectID = $row ['project_id'];
+					$Project->projectName = $row ['project_name'];
+					$Project->projectDescription = $row ['description'];
+					$Project->projectArchived = $row ['archived'];
+					$start = $row ['start'];
+					$start = strtotime($start);
+					$Project->projectStart = date("F j,Y", $start);
+					$deadline = $row ['deadline'];
+					$deadline = strtotime($deadline);
+					$Project->projectDeadline = date("F j,Y", $deadline);
+					$Projects[] = $Project;
+					/*$tasks =  "SELECT `task_id`, COUNT(task_id) FROM `tasks` WHERE `project_id` = '$project_id'";
+					$tasks = mysql_query($tasks);
+					$tasks = mysql_result($tasks,0,1);
+					*/				
+					/*if($archived == 0){
+						echo "<tr>
+							<td>$project_name</td>
+							<td>$start</td>
+							<td>$tasks</td>
+							<td>$deadline</td>
+							<td>$description</td>
+							<!--<td><a class='btn btn-primary' href='?tab=myprojects&project=$project_id'>Select</a></td>-->
+						</tr>";
+					}*/
+			}
+		return $Projects;
+	}
 	
 	public function Statistics($statistic){
 		switch ($statistic) {
